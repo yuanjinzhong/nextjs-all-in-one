0038692bdc32fc5438a6c0a39b4d1f7707078573
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2024-10-10 15:13:38 +0800

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",   /* 告诉tailwind css 这些路径下的 tailwind css 需要转换成原生的css */
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", /* 告诉tailwind css 这些路径下的 tailwind css 需要转换成原生的css */
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",  /* 告诉tailwind css 这些路径下的 tailwind css 需要转换成原生的css */
   ],
   theme: {
     extend: {
