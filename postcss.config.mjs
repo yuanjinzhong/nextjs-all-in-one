9780106e43fc931dceb23735f29a078f1941d848
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2024-10-14 19:03:09 +0800

    tailwind css 初体验释

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,10 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    //自动为 CSS 属性添加浏览器前缀，以确保你的 CSS 能够在不同浏览器上兼容。
+    // 现代浏览器对于某些新特性需要前缀来支持，
+    // autoprefixer 能够根据你定义的浏览器兼容性要求自动添加这些前缀，而不需要你手动编写。
+    autoprefixer: {},
   },
 };
 
--- src/app/tailwind/page.tsx
+++ src/app/tailwind/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import React from "react";
+
 export default function Home() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <React.Fragment>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                <div className="shrink-0">
+                    <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">ChitChat</div>
+                    <p className="text-slate-500">You have a new message!</p>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
