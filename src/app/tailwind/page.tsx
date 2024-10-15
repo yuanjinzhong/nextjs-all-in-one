b470b72a8bdd7af2058cd2e1fb8ac83e8e37c95a
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2024-10-15 20:08:06 +0800

    nextjs中的静态资源需要放在更目录下的public目录

--- src/app/tailwind/page.tsx
+++ src/app/tailwind/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
             <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
                 <div className="shrink-0">
-                    <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo"/>
+                    <img className="h-12 w-12" src="/logo.svg" alt="ChitChat Logo"/>
                 </div>
                 <div>
                     <div className="text-xl font-medium text-black">ChitChat</div>
