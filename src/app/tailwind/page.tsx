b0eb6ccef93b985943bfe7699a5d903e4033f1a2
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2024-10-15 20:09:07 +0800

    nextjs中的静态资源需要放在更目录下的public目录+1

--- src/app/tailwind/page.tsx
+++ src/app/tailwind/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
 
             <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
                 <div className="shrink-0">
+                    {/*在 Next.js 中，所有静态资源（如图片、字体、图标等）应该放置在 /public 文件夹中(根目录下的public文件夹)。Next.js 默认只能访问 public 目录下的静态资源*/}
                     <img className="h-12 w-12" src="/logo.svg" alt="ChitChat Logo"/>
                 </div>
                 <div>
