21e75de83a5a37d55f68abd1b30ef64d2b5c8bc3
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2024-11-05 18:43:08 +0800

    init github page 2

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,13 +1,14 @@
 const github = () => {
     return (
-        <div className="flex justify-center items-center mt-5">
+        <div className="flex justify-center items-center mt-5 opacity-30">
 
-            <p className="bg-slate-100 text-opacity-10 text-2xl text-red-500 font-semibold">
+            <p className="bg-slate-100  text-2xl  text-red-500 font-semibold">
                 github项目页面
             </p>
 
 
-        </div>);
+        </div>
+    );
 }
 
 export default github;
